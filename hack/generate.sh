34eccf355c651d316e95938913f85b8a277c1d8e
Shelly Kagan <skagan@redhat.com>
2025-05-27 09:55:06 +0300

    Backup API: introduce new backup api resources
    Add backupoptions struct to be used when passing
    a backup request from the backup controller through
    a vmi subresource to virt-handler to virt-launcher.
    This info will be used to performs backup and
    incremental backup by libvirt.
    
    Signed-off-by: Shelly Kagan <skagan@redhat.com>

--- hack/generate.sh
+++ hack/generate.sh
@@ -20,6 +20,7 @@ swagger-doc -in ${KUBEVIRT_DIR}/staging/src/kubevirt.io/api/export/v1alpha1/type
 swagger-doc -in ${KUBEVIRT_DIR}/staging/src/kubevirt.io/api/export/v1beta1/types.go
 swagger-doc -in ${KUBEVIRT_DIR}/staging/src/kubevirt.io/api/clone/v1alpha1/types.go
 swagger-doc -in ${KUBEVIRT_DIR}/staging/src/kubevirt.io/api/clone/v1beta1/types.go
+swagger-doc -in ${KUBEVIRT_DIR}/staging/src/kubevirt.io/api/backup/v1alpha1/types.go
 
 deepcopy-gen \
     --bounding-dirs kubevirt.io/api \
@@ -34,6 +35,7 @@ deepcopy-gen \
     kubevirt.io/api/migrations/v1alpha1 \
     kubevirt.io/api/clone/v1alpha1 \
     kubevirt.io/api/clone/v1beta1 \
+    kubevirt.io/api/backup/v1alpha1 \
     kubevirt.io/api/core/v1
 
 defaulter-gen \
@@ -62,6 +64,7 @@ openapi-gen \
     kubevirt.io/api/pool/v1alpha1 \
     kubevirt.io/api/snapshot/v1alpha1 \
     kubevirt.io/api/snapshot/v1beta1 \
+    kubevirt.io/api/backup/v1alpha1 \
     kubevirt.io/containerized-data-importer-api/pkg/apis/core/v1beta1
 
 conversion-gen \
@@ -80,7 +83,7 @@ fi
 
 client-gen --clientset-name kubevirt \
     --input-base kubevirt.io/api \
-    --input core/v1,export/v1alpha1,export/v1beta1,snapshot/v1alpha1,snapshot/v1beta1,instancetype/v1beta1,pool/v1alpha1,migrations/v1alpha1,clone/v1alpha1,clone/v1beta1 \
+    --input core/v1,export/v1alpha1,export/v1beta1,snapshot/v1alpha1,snapshot/v1beta1,instancetype/v1beta1,pool/v1alpha1,migrations/v1alpha1,clone/v1alpha1,clone/v1beta1,backup/v1alpha1 \
     --output-dir ${KUBEVIRT_DIR}/staging/src/kubevirt.io/client-go \
     --output-pkg ${CLIENT_GEN_BASE} \
     --go-header-file ${KUBEVIRT_DIR}/hack/boilerplate/boilerplate.go.txt
@@ -148,6 +151,9 @@ deepcopy-gen \
     #include clone
     GOFLAGS= controller-gen crd paths=../api/clone/v1alpha1/
     GOFLAGS= controller-gen crd paths=../api/clone/v1beta1/
+
+    #include backup
+    GOFLAGS= controller-gen crd paths=../api/backup/v1alpha1/
 
     #remove some weird stuff from controller-gen
     cd config/crd
